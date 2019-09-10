cb07d343d320c9c39318b2be03422f4e26d55c20
michayal <michayal@tamu.edu>
2019-09-10 11:07:26 -0500

    New Input method works, pushing commit

--- data.js
+++ data.js
@@ -11,100 +11,106 @@ var items = [
     {x: 2, y: 10, z: 6, size: 1.5, color: "#00ff00"}
 ];
 
-var Node = [ 
-    { nodeName: 'Node1', x: 0, y: 2.0, z: 2.5, fixedX: 1, fixedY: 1 },
-    { nodeName: 'Node2', x: 1, y: 2.0, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node3', x: 2, y: 2.0, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node4', x: 0, y: 1.0, z: 2.5, fixedX: 1, fixedY: 0 },
-    { nodeName: 'Node5', x: 1, y: 1.0, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node6', x: 2, y: 1.0, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node7', x: 0.5, y: 1.5, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node8', x: 1.5, y: 1.5, z: 2.5, fixedX: 0, fixedY: 0 },
-    { nodeName: 'Node9', x: 2.5, y: 1.5, z: 2.5, fixedX: 0, fixedY: 0 } 
-];
+var Node = 
+    [ { nodeName: 'Node0',
+       x: 0,
+       y: 2,
+       z: 2.5,
+       fixedX: 1,
+       fixedY: 1,
+       forceX: 0,
+       forceY: 0,
+      fdist: 0},
+     { nodeName: 'Node1',
+      x: 1,
+      y: 2,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node2',
+      x: 2,
+      y: 2,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node3',
+      x: 0,
+      y: 1,
+      z: 2.5,
+      fixedX: 1,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node4',
+      x: 1,
+      y: 1,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node5',
+      x: 2,
+      y: 1,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node6',
+      x: 0.1,
+      y: 1.5,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node7',
+      x: 1.5,
+      y: 1.5,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node8',
+      x: 2.5,
+      y: 1.5,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      forceX: 0,
+      forceY: -50,
+      fdist: 0 } ];
 
-var Elem = [ 
-    { elemName: 'Elem1', nodeA: 0, nodeB: 1},
-    { elemName: 'Elem2', nodeA: 1, nodeB: 2},
-    { elemName: 'Elem3', nodeA: 2, nodeB: 8},
-    { elemName: 'Elem4', nodeA: 8, nodeB: 5},
-    { elemName: 'Elem5', nodeA: 5, nodeB: 4},
-    { elemName: 'Elem6', nodeA: 4, nodeB: 3},
-    { elemName: 'Elem7', nodeA: 3, nodeB: 6},
-    { elemName: 'Elem8', nodeA: 6, nodeB: 0},
-    { elemName: 'Elem9', nodeA: 6, nodeB: 1},
-    { elemName: 'Elem10', nodeA: 6, nodeB: 4},
-    { elemName: 'Elem11', nodeA: 1, nodeB: 4},
-    { elemName: 'Elem12', nodeA: 1, nodeB: 7},
-    { elemName: 'Elem13', nodeA: 4, nodeB: 7},
-    { elemName: 'Elem14', nodeA: 7, nodeB: 2},
-    { elemName: 'Elem15', nodeA: 7, nodeB: 5},
-    { elemName: 'Elem16', nodeA: 2, nodeB: 5} 
-];
-/*
-function spawnnodes() {
-
-    const fs = requirejs('fs'); 
-    const map = Array.prototype.map;
-    // Requiring fs module in which 
-    // readFile function is defined. 
-    var content = new Array();
-    // Reading data in utf-8 format 
-    // which is a type of character set. 
-    // Instead of 'utf-8' it can be  
-    // other character set also like 'ascii' 
-
-    fs.readFileSync('Input2.csv', 'utf-8');
-    console.log(data);
-    var cells = data.split(',');
-    content = Array.from(cells);
-
-    // Invoke the next step here however you like
-    // Put all of the code here (not the best solution)
-    processFile();
-
-    function processFile() {
-        //console.log(content);
-
-        const newNum = map.call(content, Number => {
-            return {Number}
-        });
-
-        //this.scene = document.getElementById('a-scene');
-
-        //var scene =  document.querySelector('a-scene').object3D;
-        //var scene = this.el.sceneEl.object3D;
-        var i;
-        var num = 0;
-        var Node = [];
-        var Elem = [];
-        for (i = 1; i < content.length; i = i+6, num = num+1) { 
-            if (content[i] != '\n*Element'){
-                //Node Objects are created and characterized here
-                var nodeInfo = {nodeName : 'Node'+ String(Number(content[i])), x : Number(content[i+1]),
-                                y : Number(content[i+2]), z : Number(content[i+3]),
-                                fixedX : Number(content[i+4]), fixedY : Number(content[i+5])};
-                Node.push(nodeInfo);
-                console.log(nodeInfo);
-                console.log(Node);
-
-
-            }
-            else if(content[i] == '\n*Element'){ 
-                for (var i = i + 1, num = 0; i < content.length; i = i+5, num = num+1) { 
-                    //Element Objects are created and characterized here
-                    Elem[num] = { elemName : 'Elem'+ String(Number(content[i])), nodeA : Number(content[i+1]),
-                                 nodeB : Number(content[i+2]), diam : Number(content[i+3]),
-                                 modulus : Number(content[i+4])}};
-                //break;
-            };
-        };
-
-
-
-
-    };
-};*/
+var Elem = 
+    [ { elemName: 'Elem0', nodeA: 0, nodeB: 1 },
+     { elemName: 'Elem1', nodeA: 1, nodeB: 2 },
+     { elemName: 'Elem2', nodeA: 2, nodeB: 8 },
+     { elemName: 'Elem3', nodeA: 8, nodeB: 5 },
+     { elemName: 'Elem4', nodeA: 5, nodeB: 4 },
+     { elemName: 'Elem5', nodeA: 4, nodeB: 3 },
+     { elemName: 'Elem6', nodeA: 3, nodeB: 6 },
+     { elemName: 'Elem7', nodeA: 6, nodeB: 0 },
+     { elemName: 'Elem8', nodeA: 6, nodeB: 1 },
+     { elemName: 'Elem9', nodeA: 6, nodeB: 4 },
+     { elemName: 'Elem10', nodeA: 1, nodeB: 4 },
+     { elemName: 'Elem11', nodeA: 1, nodeB: 7 },
+     { elemName: 'Elem12', nodeA: 4, nodeB: 7 },
+     { elemName: 'Elem13', nodeA: 7, nodeB: 2 },
+     { elemName: 'Elem14', nodeA: 7, nodeB: 5 },
+     { elemName: 'Elem15', nodeA: 2, nodeB: 5 } ];
 
 function plotAxis (scene, color) {
     var axis = document.createElement('a-entity');
@@ -191,20 +197,12 @@ function myPrint(){
 
 function doAnalysis(){
     // Node[0].DOF = 2; //Adding a value-pair to a JSON object
-    //console.log(Node[0]);
 
     var numElem = Elem.length;
     var numNodes = Node.length;
-    var elemCoords = math.zeros(numNodes,2);
-
-    for (var i = 0; i < numNodes; i = i+1) {
-        elemCoords = math.subset(elemCoords,math.index(i,0),Node[i].x);
-        elemCoords = math.subset(elemCoords,math.index(i,1),Node[i].y);
-
-        //elemCoords[i,1] = Node[i].x;
-        //elemCoords[i,2] = Node[i].y;
-    }
-    //console.log(elemCoords);
+    
+    var Kglobal = math.zeros(numNodes*2, numNodes*2);
+    var Qglobal = math.zeros(numNodes*2,1);
 
     var elemDOFs = math.zeros(numElem, 4);
     var elemLengths = math.zeros(1,numElem);
@@ -212,56 +210,38 @@ function doAnalysis(){
 
     //Element and Node Connectivity defined here
     for (var i = 0; i < numElem; i = i+1) {
-        //console.log((Elem[i].nodeA+1)*2);
         elemDOFs = math.subset(elemDOFs,math.index(i,0),(Elem[i].nodeA+1)*2-2);
         elemDOFs = math.subset(elemDOFs,math.index(i,1),(Elem[i].nodeA+1)*2-1);
         elemDOFs = math.subset(elemDOFs,math.index(i,2),(Elem[i].nodeB+1)*2-2);
         elemDOFs = math.subset(elemDOFs,math.index(i,3),(Elem[i].nodeB+1)*2-1);
 
-        //console.log(Node[Elem[i].nodeB].x);
         elemLengths[i] = math.sqrt(math.square(Node[Elem[i].nodeB].x - Node[Elem[i].nodeA].x) + math.square(Node[Elem[i].nodeB].y - Node[Elem[i].nodeA].y));
-        //console.log(elemLengths[i]);
 
     }
-    //console.log(elemLengths);
-    //console.log(elemDOFs);
-    //console.log(elemLengths);
 
     //Problem Parameters defined here
     var EA = 2003;
     var A = 1; //Is this neccesary? What does it do?
 
-    //Displacement Boundary Conditions defined here
-    var dispBCs = math.matrix([[0,0],[1,0],[6,0]]);
-
-    //Force BCs defined here
-    var forceBCs = [17,-150]; //Force of -15 at node9 aka DOF17 which is -y direction
-
-    var f_dist_nodal = math.matrix([[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0]])
-    var f_dist = math.zeros(1,9);
+    var dispBCs = math.zeros(numNodes*2, 1);
+    for (var i = 0, num = 0; i < numNodes*2; i = i+2, num = num+1) {
+        // Encodes dispBCs from Nodal data
+        dispBCs.subset(math.index(i,0),Node[num].fixedX);
+        dispBCs.subset(math.index(i+1,0),Node[num].fixedY);
+        
+        //Encodes Global Q matrix from Nodal data
+        Qglobal.subset(math.index(i,0),Node[num].forceX);
+        Qglobal.subset(math.index(i+1,0),Node[num].forceY);
+    }
+    
+    //Encodes f_dist from Nodal data
+    var f_dist = math.zeros(1,numNodes);
     for (var i = 0; i < 9; i = i+1) {
 
-        f_dist = math.subset(f_dist,math.index(0,i),math.subset(f_dist_nodal, math.index(i,1)));
+        f_dist.subset(math.index(0,i),Node[i].fdist);
     };
-    //console.log(f_dist);
 
-    //Initialize global K & Q Matrices
-    var Kglobal = math.zeros(numNodes*2, numNodes*2);
-    var Qglobal = math.zeros(numNodes*2,1);
-
-
-    // console.log(forceBCs[0]); //Where the force is applied, actually this minus 1 bc "matlab"
-    // *Pls figure out a more efficient way to add forces to Qglobal than this hardcoded method*
-    for (var i = 0; i < numNodes*2; i = i+1) {
-        if( i == (forceBCs[0])){
-            Qglobal.subset(math.index(i,0),forceBCs[1]); 
-        }
-        else{
-            Qglobal.subset(math.index(i,0),0);
-        }
-    }
-
-
+    //Initialize global K Matrices
     for (var i = 0; i < numElem; i = i+1) {
         // Create and assemble element stiffness matrix
         var node1 = Elem[i].nodeA;
@@ -309,22 +289,21 @@ function doAnalysis(){
 
     // Enforce Displacement BCs through penalty method
 
-    var numConds = dispBCs._size[0];
-    for (var i = 0; i < numConds; i = i+1) {
+    for (var i = 0; i < dispBCs._size[0]; i = i+1) {
         var BCindex = math.subset(dispBCs,math.index(i,0));
-        var BCfactor = math.subset(dispBCs,math.index(i,1));
-        Kglobal.subset(math.index(BCindex,BCindex),math.multiply(math.subset(Kglobal,math.index(BCindex,BCindex)),1E15));
-        //console.log(math.multiply(math.subset(Kglobal,math.index(BCindex,BCindex)),BCfactor));
-        math.subset(Qglobal,math.index(BCindex,0),math.multiply(math.subset(Kglobal,math.index(BCindex,BCindex)),BCfactor));
-        //Qglobal.subset(math.index(BCindex,0),math.multiply(math.subset(Kglobal,math.index(BCindex,BCindex)),BCfactor));
+        //if Node is fixed
+        if (BCindex == 1){
+            Kglobal.subset(math.index(i,i),math.multiply(math.subset(Kglobal,math.index(i,i)),1E15)); //Multiplies ii in Kglobal by 1E15 if fixed
+            math.subset(Qglobal,math.index(i,0),math.multiply(math.subset(Kglobal,math.index(i,i)),0)); //Cancels out forces at node if fixed
+        }
     }
 
     //Solve for qGobal
     var Kinv = math.inv(Kglobal)
     //var Kinv2 = denseMatrix.create(Kinv, [Number]);
-    console.log(Qglobal);
-    console.log(Kinv);
-    var qGlobal = math.multiply(Kinv,Qglobal); //This is where your code is getting wonky, where does all the math go??
+    //console.log(Qglobal);
+    //console.log(Kinv);
+    var qGlobal = math.multiply(Kinv,Qglobal); 
     console.log(qGlobal);
 
     var stress = math.zeros(numElem,1);
@@ -355,8 +334,8 @@ function doAnalysis(){
     }
 
 
-    //console.log(Qglobal);
-    //console.log(Kglobal);
+    console.log(Qglobal);
+    console.log(Kglobal);
     //console.log(stress._size[0]);
 
     //Solve for buckling
@@ -378,13 +357,13 @@ function doAnalysis(){
     for (i = 0; i < numNodes; i = i+1) { 
         //Node Objects are created and characterized here
         DefNode[i] = { DefnodeName : 'DefNode'+ String(i), x : math.subset(deformedNodes,math.index(i,0)),
-                        y : math.subset(deformedNodes,math.index(i,1)), z : Node[i].z};
+                      y : math.subset(deformedNodes,math.index(i,1)), z : Node[i].z};
     }
 
     //console.log(DefNode);
     var scene = document.querySelector('a-scene');
     var detailText = document.getElementById('detailText');
-    
+
     var i = 0;
     for (let item of DefNode) {
         //console.log(Node[i].x);
@@ -448,7 +427,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     //plotAxis (scene, 'black')
     doAnalysis();
-    
-    
-    
+
+
+
 });
--- readInput.js
+++ readInput.js
@@ -10,7 +10,7 @@ var content = new Array();
 
 fs.readFile('306Truss.csv', 'utf-8', (err, data) => { 
     if (err) {throw err; }
-  
+
     // Converting Raw Buffer to text 
     // data using tostring function. 
     var cells = data.split(',');
@@ -23,40 +23,43 @@ fs.readFile('306Truss.csv', 'utf-8', (err, data) => {
 
 function processFile() {
     //console.log(content);
-  
-  const newNum = map.call(content, Number => {
-      return {Number}
+
+    const newNum = map.call(content, Number => {
+        return {Number}
     });
-  
-  //this.scene = document.getElementById('a-scene');
-  
-  var i;
-  var num = 0;
-  var Node = [];
-  var Elem = [];
-  for (i = 1; i < content.length; i = i+6, num = num+1) { 
-      if (content[i] != '\n*Element'){
-        //Node Objects are created and characterized here
-        Node[num] = { nodeName : 'Node'+ String(Number(content[i])), x : Number(content[i+1])*100,
-                   y : Number(content[i+2])*100 + 2.0, z : Number(content[i+3])*10,
-                   fixedX : Number(content[i+4]), fixedY : Number(content[i+5])};
-        //NodeObj =  document.createElement('a-entity');
-        //NodeObj.setAttribute('Node','NodeNum :' Number(content[i])';' 'Pos:  '{Number(content[i+1]) Number(content[i+2]) Number(content[i+3])})
-        //this.scene.appendChild(NodeObj)
-        
-        
-           }
-      else if(content[i] == '\n*Element'){ 
-        for (var i = i + 1, num = 0; i < content.length; i = i+5, num = num+1) { 
-          //Element Objects are created and characterized here
-            Elem[num] = { elemName : 'Elem'+ String(Number(content[i])), nodeA : Number(content[i+1]),
-                   nodeB : Number(content[i+2]), diam : Number(content[i+3]),
-                   modulus : Number(content[i+4])};}
-        //break;
+
+    //this.scene = document.getElementById('a-scene');
+
+    var i;
+    var num = 0;
+    var Node = [];
+    var Elem = [];
+    for (i = 1; i < content.length; i = i+8, num = num+1) { 
+        if (content[i] != '\n*Element'){
+            //Node Objects are created and characterized here
+            Node[num] = { nodeName : 'Node'+ String(Number(content[i])), x : Number(content[i+1]),
+                         y : Number(content[i+2]), z : Number(content[i+3]),
+                         fixedX : Number(content[i+4]), fixedY : Number(content[i+5]),
+                         forceX : Number(content[i+6]), forceY : Number(content[i+7]),
+                         fdist : Number(content[i+7])};
+            //NodeObj =  document.createElement('a-entity');
+            //NodeObj.setAttribute('Node','NodeNum :' Number(content[i])';' 'Pos:  '{Number(content[i+1]) Number(content[i+2]) Number(content[i+3])})
+            //this.scene.appendChild(NodeObj)
+
+
+        }
+        else if(content[i] == '\n*Element'){ 
+            for (var i = i + 1, num = 0; i < content.length; i = i+3, num = num+1) { 
+                //Element Objects are created and characterized here
+                Elem[num] = { elemName : 'Elem'+ String(Number(content[i])), nodeA : Number(content[i+1]),
+                             nodeB : Number(content[i+2])}};
+            //break;
+        }
     }
-  }
-  
-  
-  console.log('var Node = ' Node);
-  console.log('var Elem = ' Elem);
+
+
+    console.log('var Node = ');
+    console.log(Node);
+    console.log('var Elem = ');
+    console.log(Elem);
 }
