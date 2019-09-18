9ed3182e31c34a1d49419651a21235ed178f7253
michayal <michayal@tamu.edu>
2019-09-18 13:29:17 -0500

    FrameV1
    Seems to be able to solve FrameFEA problems now

--- data.js
+++ data.js
@@ -1,116 +1,136 @@
-//import math from 'mathjs'
+//This is the 2D Truss script extended out to 2D Frame calculations
 
-var items = [
-    {x: 2, y: 8, z: 0, size: 1, color: "#ff0000"},
-    {x: -2, y: 3, z: 1, size: 1.5, color: "#00ff00"},
-    {x: 3, y: 3, z: 2, size: 1, color: "#0000ff"},
-    {x: 5, y: 7, z: 7, size: 1.5, color: "#0000ff"},
-    {x: 1, y: 6, z: 3, size: 1, color: "#4CC3D9"},
-    {x: 5, y: 4, z: 1, size: 1.5, color: "#EF2D5E"},
-    {x: 4, y: 1, z: 5, size: 1, color: "#ff0000"},
-    {x: 2, y: 10, z: 6, size: 1.5, color: "#00ff00"}
-];
-
-var Node = 
+var Node =
     [ { nodeName: 'Node0',
-       x: 0,
-       y: 2,
+       x: 2,
+       y: 2.5,
        z: 2.5,
        fixedX: 1,
        fixedY: 1,
+       fixedM: 1,
        forceX: 0,
        forceY: 0,
-      fdist: 0},
+       fdist: 0 },
      { nodeName: 'Node1',
-      x: 1,
-      y: 2,
+      x: 3.06801,
+      y: 2.80121,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node2',
-      x: 2,
-      y: 2,
+      x: 3.97127,
+      y: 2.66541,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node3',
-      x: 0,
-      y: 1,
-      z: 2.5,
-      fixedX: 1,
-      fixedY: 0,
-      forceX: 0,
-      forceY: 0,
-      fdist: 0 },
-     { nodeName: 'Node4',
-      x: 1,
-      y: 1,
+      x: 5.00181,
+      y: 2,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
+      forceX: 0,
+      forceY: -3500,
+      fdist: 0 },
+     { nodeName: 'Node4',
+      x: 2.774363,
+      y: 2,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      fixedM: 0,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node5',
-      x: 2,
-      y: 1,
+      x: 3.00012,
+      y: 2.34964,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node6',
-      x: 0.1,
-      y: 1.5,
+      x: 3.54162,
+      y: 2,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node7',
-      x: 1.5,
+      x: 2,
       y: 1.5,
       z: 2.5,
-      fixedX: 0,
-      fixedY: 0,
+      fixedX: 1,
+      fixedY: 1,
+      fixedM: 1,
       forceX: 0,
       forceY: 0,
       fdist: 0 },
      { nodeName: 'Node8',
-      x: 2.5,
-      y: 1.5,
+      x: 3.06801,
+      y: 1.19879,
       z: 2.5,
       fixedX: 0,
       fixedY: 0,
+      fixedM: 0,
       forceX: 0,
-      forceY: -50,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node9',
+      x: 3.97127,
+      y: 1.33459,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      fixedM: 0,
+      forceX: 0,
+      forceY: 0,
+      fdist: 0 },
+     { nodeName: 'Node10',
+      x: 3.00012,
+      y: 1.65036,
+      z: 2.5,
+      fixedX: 0,
+      fixedY: 0,
+      fixedM: 0,
+      forceX: 0,
+      forceY: 0,
       fdist: 0 } ];
 
-var Elem = 
+var Elem =
     [ { elemName: 'Elem0', nodeA: 0, nodeB: 1 },
      { elemName: 'Elem1', nodeA: 1, nodeB: 2 },
-     { elemName: 'Elem2', nodeA: 2, nodeB: 8 },
-     { elemName: 'Elem3', nodeA: 8, nodeB: 5 },
-     { elemName: 'Elem4', nodeA: 5, nodeB: 4 },
-     { elemName: 'Elem5', nodeA: 4, nodeB: 3 },
-     { elemName: 'Elem6', nodeA: 3, nodeB: 6 },
-     { elemName: 'Elem7', nodeA: 6, nodeB: 0 },
-     { elemName: 'Elem8', nodeA: 6, nodeB: 1 },
-     { elemName: 'Elem9', nodeA: 6, nodeB: 4 },
-     { elemName: 'Elem10', nodeA: 1, nodeB: 4 },
-     { elemName: 'Elem11', nodeA: 1, nodeB: 7 },
-     { elemName: 'Elem12', nodeA: 4, nodeB: 7 },
-     { elemName: 'Elem13', nodeA: 7, nodeB: 2 },
-     { elemName: 'Elem14', nodeA: 7, nodeB: 5 },
-     { elemName: 'Elem15', nodeA: 2, nodeB: 5 } ];
+     { elemName: 'Elem2', nodeA: 2, nodeB: 3 },
+     { elemName: 'Elem3', nodeA: 0, nodeB: 4 },
+     { elemName: 'Elem4', nodeA: 0, nodeB: 5 },
+     { elemName: 'Elem5', nodeA: 4, nodeB: 5 },
+     { elemName: 'Elem6', nodeA: 1, nodeB: 5 },
+     { elemName: 'Elem7', nodeA: 5, nodeB: 6 },
+     { elemName: 'Elem8', nodeA: 6, nodeB: 2 },
+     { elemName: 'Elem9', nodeA: 7, nodeB: 8 },
+     { elemName: 'Elem10', nodeA: 8, nodeB: 9 },
+     { elemName: 'Elem11', nodeA: 9, nodeB: 3 },
+     { elemName: 'Elem12', nodeA: 7, nodeB: 4 },
+     { elemName: 'Elem13', nodeA: 7, nodeB: 10 },
+     { elemName: 'Elem14', nodeA: 4, nodeB: 10 },
+     { elemName: 'Elem15', nodeA: 8, nodeB: 10 },
+     { elemName: 'Elem16', nodeA: 10, nodeB: 6 },
+     { elemName: 'Elem17', nodeA: 6, nodeB: 9 } ];
 
 function plotDot (scene, position, size, color, id, text) {
     var sphere = document.createElement('a-sphere');
@@ -166,49 +186,61 @@ function myPrint(){
     console.log("SOmething random")
 };
 
-function doAnalysis(){
+function InitAnalysis(){
     // Node[0].DOF = 2; //Adding a value-pair to a JSON object
 
     var numElem = Elem.length;
     var numNodes = Node.length;
-    
-    var Kglobal = math.zeros(numNodes*2, numNodes*2);
-    var Qglobal = math.zeros(numNodes*2,1);
 
-    var elemDOFs = math.zeros(numElem, 4);
+    var Kglobal = math.zeros(numNodes*3, numNodes*3);
+    var Qglobal = math.zeros(numNodes*3,1);
+
+    var elemDOFs = math.zeros(numElem, 6);
     var elemLengths = math.zeros(1,numElem);
     var newElemLen;
 
     //Element and Node Connectivity defined here
     for (var i = 0; i < numElem; i = i+1) {
-        elemDOFs = math.subset(elemDOFs,math.index(i,0),(Elem[i].nodeA+1)*2-2);
-        elemDOFs = math.subset(elemDOFs,math.index(i,1),(Elem[i].nodeA+1)*2-1);
-        elemDOFs = math.subset(elemDOFs,math.index(i,2),(Elem[i].nodeB+1)*2-2);
-        elemDOFs = math.subset(elemDOFs,math.index(i,3),(Elem[i].nodeB+1)*2-1);
+        elemDOFs = math.subset(elemDOFs,math.index(i,0),(Elem[i].nodeA+1)*3-3); //Node 1 xDOF
+        elemDOFs = math.subset(elemDOFs,math.index(i,1),(Elem[i].nodeA+1)*3-2); //Node 1 yDOF
+        elemDOFs = math.subset(elemDOFs,math.index(i,2),(Elem[i].nodeA+1)*3-1); //Node 1 mDOF
+        elemDOFs = math.subset(elemDOFs,math.index(i,3),(Elem[i].nodeB+1)*3-3); //Node 2 xDOF
+        elemDOFs = math.subset(elemDOFs,math.index(i,4),(Elem[i].nodeB+1)*3-2); //Node 2 yDOF
+        elemDOFs = math.subset(elemDOFs,math.index(i,5),(Elem[i].nodeB+1)*3-1); //Node 2 mDOF
 
-        elemLengths[i] = math.sqrt(math.square(Node[Elem[i].nodeB].x - Node[Elem[i].nodeA].x) + math.square(Node[Elem[i].nodeB].y - Node[Elem[i].nodeA].y));
+        elemLengths[i] = math.sqrt(math.square(Node[Elem[i].nodeB].x - Node[Elem[i].nodeA].x) + math.square(Node[Elem[i].nodeB].y - Node[Elem[i].nodeA].y) + math.square(Node[Elem[i].nodeB].z - Node[Elem[i].nodeA].z));
 
     }
 
     //Problem Parameters defined here
-    var EA = 2003;
-    var A = 1; //Is this neccesary? What does it do?
+    var E = 1.5E9;
+    var t = 0.002;
+    var w = 0.02;
+    var fx = 0;
+    var fy = 0;
+    var A = t*w;
+    var I = 1/12*w*math.pow(t,3);
+    var EI = E*I;
+    var EA = E*A;
 
     var dispBCs = math.zeros(numNodes*2, 1);
-    for (var i = 0, num = 0; i < numNodes*2; i = i+2, num = num+1) {
+    for (var i = 0, num = 0; i < numNodes*3; i = i+3, num = num+1) {
         // Encodes dispBCs from Nodal data
         dispBCs.subset(math.index(i,0),Node[num].fixedX);
         dispBCs.subset(math.index(i+1,0),Node[num].fixedY);
-        
+        dispBCs.subset(math.index(i+2,0),Node[num].fixedM);
+
         //Encodes Global Q matrix from Nodal data
         Qglobal.subset(math.index(i,0),Node[num].forceX);
         Qglobal.subset(math.index(i+1,0),Node[num].forceY);
     }
-    
+    console.log(dispBCs);
+    console.log(Qglobal);
+
+
     //Encodes f_dist from Nodal data
     var f_dist = math.zeros(1,numNodes);
-    for (var i = 0; i < 9; i = i+1) {
-
+    for (var i = 0; i < numNodes; i = i+1) {
         f_dist.subset(math.index(0,i),Node[i].fdist);
     };
 
@@ -219,44 +251,47 @@ function doAnalysis(){
         var node2 = Elem[i].nodeB;
         var c = (Node[node2].x - Node[node1].x)/elemLengths[i];
         var s = (Node[node2].y - Node[node1].y)/elemLengths[i];
-        var T = math.matrix([[c,s,0,0],[-s,c,0,0],[0,0,c,s],[0,0,-s,c]]);
-        var Kelem = math.matrix([[1,0,-1,0],[0,0,0,0],[-1,0,1,0],[0,0,0,0]]);
-        Kelem = math.multiply(Kelem,(EA/elemLengths[i]));
-        Kelem = math.multiply(math.multiply(math.transpose(T),Kelem),T);
+        var T = math.matrix([[c,s,0,0,0,0],[-s,c,0,0,0,0],[0,0,1,0,0,0],[0,0,0,c,s,0],[0,0,0,-s,c,0],[0,0,0,0,0,1]]);
 
-        //console.log(Kelem);
+        //Do all the math here and assign them to the correct indices later.
+        var L = elemLengths[i];
+        var Lsquared = math.pow(L,2);
+        var Lcubed = math.pow(L,3);
+        var csquared = math.pow(c,2);
+        var ssquared = math.pow(s,2);
 
-        for (var j = 0; j < 4; j = j+1) {
-            for (var k = 0; k < 4; k = k+1) {
+        var Kelem = math.matrix([[csquared*EA/L + 12*ssquared*EI/Lcubed, c*EA*s/L - 12*s*c*EI/Lcubed, -6*s*EI/Lsquared, -csquared*EA/L - 12*ssquared*EI/Lcubed, -c*EA*s/L + 12*s*c*EI/Lcubed, -6*s*EI/Lsquared],
+                                 [c*EA*s/L - 12*s*c*EI/Lcubed, ssquared*EA/L + 12*csquared*EI/Lcubed, 6*c*EI/Lsquared, -c*EA*s/L + 12*s*c*EI/Lcubed, -ssquared*EA/L - 12*csquared*EI/Lcubed, 6*c*EI/Lsquared],
+                                 [-6*s*EI/Lsquared, 6*c*EI/Lsquared, 4*EI/L, 6*s*EI/Lsquared, -6*c*EI/Lsquared, 2*EI/L ],
+                                 [-csquared*EA/L - 12*ssquared*EI/Lcubed, -c*EA*s/L + 12*s*c*EI/Lcubed, 6*s*EI/Lsquared, csquared*EA/L + 12*ssquared*EI/Lcubed, c*EA*s/L - 12*s*c*EI/Lcubed, 6*s*EI/Lsquared],
+                                 [-c*EA*s/L + 12*s*c*EI/Lcubed, -ssquared*EA/L - 12*csquared*EI/Lcubed, -6*c*EI/Lsquared, c*EA*s/L - 12*s*c*EI/Lcubed, ssquared*EA/L + 12*csquared*EI/Lcubed, -6*c*EI/Lsquared],
+                                 [-6*s*EI/Lsquared, 6*c*EI/Lsquared, 2*EI/L, 6*s*EI/Lsquared, -6*c*EI/Lsquared, 4*EI/L]]);
+
+        
+        // Create and assemble Distributed Force Vector
+        var Qdist = math.zeros(6,1);
+        Qdist.subset(math.index(0,0),L*fx/2);
+        Qdist.subset(math.index(1,0),L*fy/2);
+        Qdist.subset(math.index(2,0),(c*fy-s*fx)*Lsquared/12);
+        Qdist.subset(math.index(3,0),L*fx/2);
+        Qdist.subset(math.index(4,0),L*fy/2);
+        Qdist.subset(math.index(5,0),-(c*fy-s*fx)*Lsquared/12);
+
+        for (var j = 0; j < 6; j = j+1) {
+            var newIndex3 = math.subset(elemDOFs,math.index(i,j)); // Finds DOF index for ith Element
+
+            var newQ = math.add(math.subset(Qglobal,math.index(newIndex3,0)), math.subset(Qdist,math.index(j,0))); // Adds Qdist to Qglobal
+            Qglobal.subset(math.index(newIndex3,0),newQ);
+
+            for (var k = 0; k < 6; k = k+1) {
                 var newIndex1 = math.subset(elemDOFs,math.index(i,j));
                 var newIndex2 = math.subset(elemDOFs,math.index(i,k));
 
                 var newK = math.add(math.subset(Kglobal,math.index(newIndex1,newIndex2)), math.subset(Kelem,math.index(j,k)));
-                //console.log(math.subset(Kelem,math.index(j,k)));
-                //console.log(newK);
                 Kglobal.subset(math.index(newIndex1,newIndex2), newK);
-                //Kglobal[elemDOFs[j,k]] = Kglobal[elemDOFs[j,k]] + Kelem[j,k];
             }
         }
-
-        // Create and assemble Force Vector
-        var f1 = math.subset(f_dist,math.index(0,node1));
-        var f2 = math.subset(f_dist,math.index(0,node2));
-        var Qelem = math.ones(4,1);
-        Qelem.subset(math.index(0,0),(1/20)*elemLengths[i]*(7*f1 + 3*f2));
-        Qelem.subset(math.index(1,0),(1/60)*math.square(elemLengths[i])*(3*f1 + 2*f2));
-        Qelem.subset(math.index(2,0),(1/20)*elemLengths[i]*(3*f1 + 7*f2));
-        Qelem.subset(math.index(3,0),-(1/60)*math.square(elemLengths[i])*(2*f1 + 3*f2));
-
-        for (var m = 0; m < 4; m = m+1) {
-            var newIndex3 = math.subset(elemDOFs,math.index(i,m)); // Finds DOF index for ith Element
-            var newQ = math.add(math.subset(Qglobal,math.index(newIndex3,0)), math.subset(Qelem,math.index(m,0))); // Adds Qelem to Qglobal
-            Qglobal.subset(math.index(newIndex3,0),newQ);      
-        }
-
     }
-
-    //console.log(Kglobal);
 
     // Enforce Displacement BCs through penalty method
 
@@ -269,44 +304,71 @@ function doAnalysis(){
         }
     }
 
-    //Solve for qGobal
+    //Solve for qGlobal
     var Kinv = math.inv(Kglobal)
-    //var Kinv2 = denseMatrix.create(Kinv, [Number]);
-    //console.log(Qglobal);
-    //console.log(Kinv);
     var qGlobal = math.multiply(Kinv,Qglobal); 
     console.log(qGlobal);
-
-    var stress = math.zeros(numElem,1);
+    
+    var stress = math.zeros(numElem,6);
     for (var i = 0; i < numElem; i = i+1) {
         var node1 = Elem[i].nodeA;
         var node2 = Elem[i].nodeB;
         var c = (Node[node2].x - Node[node1].x)/elemLengths[i];
         var s = (Node[node2].y - Node[node1].y)/elemLengths[i];
-        var T = math.matrix([[c,s,0,0],[-s,c,0,0],[0,0,c,s],[0,0,-s,c]]);
-        var Kelem = math.matrix([[1,0,-1,0],[0,0,0,0],[-1,0,1,0],[0,0,0,0]]);
-        Kelem = math.multiply(Kelem,(EA/elemLengths[i]));
-        Kelem = math.multiply(math.multiply(math.transpose(T),Kelem),T);
+        var T = math.matrix([[c,s,0,0,0,0],[-s,c,0,0,0,0],[0,0,1,0,0,0],[0,0,0,c,s,0],[0,0,0,-s,c,0],[0,0,0,0,0,1]]);
+
+        //Do all the math here and assign them to the correct indices later.
+        var L = elemLengths[i];
+        var Lsquared = math.pow(L,2);
+        var Lcubed = math.pow(L,3);
+        var csquared = math.pow(c,2);
+        var ssquared = math.pow(s,2);
+
+        var Kelem = math.matrix([[csquared*EA/L + 12*ssquared*EI/Lcubed, c*EA*s/L - 12*s*c*EI/Lcubed, -6*s*EI/Lsquared, -csquared*EA/L - 12*ssquared*EI/Lcubed, -c*EA*s/L + 12*s*c*EI/Lcubed, -6*s*EI/Lsquared],
+                                 [c*EA*s/L - 12*s*c*EI/Lcubed, ssquared*EA/L + 12*csquared*EI/Lcubed, 6*c*EI/Lsquared, -c*EA*s/L + 12*s*c*EI/Lcubed, -ssquared*EA/L - 12*csquared*EI/Lcubed, 6*c*EI/Lsquared],
+                                 [-6*s*EI/Lsquared, 6*c*EI/Lsquared, 4*EI/L, 6*s*EI/Lsquared, -6*c*EI/Lsquared, 2*EI/L ],
+                                 [-csquared*EA/L - 12*ssquared*EI/Lcubed, -c*EA*s/L + 12*s*c*EI/Lcubed, 6*s*EI/Lsquared, csquared*EA/L + 12*ssquared*EI/Lcubed, c*EA*s/L - 12*s*c*EI/Lcubed, 6*s*EI/Lsquared],
+                                 [-c*EA*s/L + 12*s*c*EI/Lcubed, -ssquared*EA/L - 12*csquared*EI/Lcubed, -6*c*EI/Lsquared, c*EA*s/L - 12*s*c*EI/Lcubed, ssquared*EA/L + 12*csquared*EI/Lcubed, -6*c*EI/Lsquared],
+                                 [-6*s*EI/Lsquared, 6*c*EI/Lsquared, 2*EI/L, 6*s*EI/Lsquared, -6*c*EI/Lsquared, 4*EI/L]]);
 
 
-        //console.log(math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,0)),0)));
-
-
-        var qelem = math.zeros(4,1);
+        var qelem = math.zeros(6,1);
         qelem.subset(math.index(0,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,0)),0)));
         qelem.subset(math.index(1,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,1)),0)));
         qelem.subset(math.index(2,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,2)),0)));
         qelem.subset(math.index(3,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,3)),0)));
+        qelem.subset(math.index(4,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,4)),0)));
+        qelem.subset(math.index(5,0),math.subset(qGlobal,math.index(math.subset(elemDOFs,math.index(i,5)),0)));
+        
+        
+        var Qdist = math.zeros(6,1);
+        Qdist.subset(math.index(0,0),L*fx/2);
+        Qdist.subset(math.index(1,0),L*fy/2);
+        Qdist.subset(math.index(2,0),(c*fy-s*fx)*Lsquared/12);
+        Qdist.subset(math.index(3,0),L*fx/2);
+        Qdist.subset(math.index(4,0),L*fy/2);
+        Qdist.subset(math.index(5,0),-(c*fy-s*fx)*Lsquared/12);
 
-        Qelem = math.multiply(T,Kelem,qelem)
-        //console.log(Qelem);
-        stress.subset(math.index(i,0),math.multiply(-A,math.subset(Qelem,math.index(0,0))));
+        var val1 = math.multiply(Kelem,qelem);
+        var GlobalForce = math.subtract(val1,Qdist);
+        var force = math.multiply(T,GlobalForce);
+        //Supposedly need to switch signs of first element?
+        force.subset(math.index(0,0),-1*force.subset(math.index(0,0)));
+        force.subset(math.index(1,0),-1*force.subset(math.index(1,0)));
+        force.subset(math.index(2,0),-1*force.subset(math.index(2,0)));
+
+        //Calculate Element Stresses
+        stress.subset(math.index(i,0),force.subset(math.index(0,0))/A - force.subset(math.index(2,0))*0.5*t/I);
+        stress.subset(math.index(i,1),force.subset(math.index(0,0))/A - force.subset(math.index(2,0))*-0.5*t/I);
+        stress.subset(math.index(i,2),force.subset(math.index(1,0))/A);
+        stress.subset(math.index(i,3),force.subset(math.index(3,0))/A - force.subset(math.index(5,0))*0.5*t/I);
+        stress.subset(math.index(i,4),force.subset(math.index(0,0))/A - force.subset(math.index(5,0))*-0.5*t/I);
+        stress.subset(math.index(i,5),force.subset(math.index(4,0))/A);
 
     }
 
 
-    console.log(Qglobal);
-    console.log(Kglobal);
+    console.log(stress);
     //console.log(stress._size[0]);
 
     //Solve for buckling
@@ -318,8 +380,8 @@ function doAnalysis(){
 
     var deformedNodes = math.zeros(numNodes,2);
     for (var i = 0; i < numNodes; i = i+1) {
-        deformedNodes.subset(math.index(i,0), Node[i].x + math.subset(qGlobal,math.index(2*i,0)));
-        deformedNodes.subset(math.index(i,1), Node[i].y + math.subset(qGlobal,math.index((2*i)+1,0)));
+        deformedNodes.subset(math.index(i,0), Node[i].x + math.subset(qGlobal,math.index(3*i,0)));
+        deformedNodes.subset(math.index(i,1), Node[i].y + math.subset(qGlobal,math.index((3*i)+1,0)));
     }
     console.log(deformedNodes);
 
@@ -397,8 +459,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     }
     //plotAxis (scene, 'black')
-    doAnalysis();
-
-
-
+    InitAnalysis();
 });
--- readInput.js
+++ readInput.js
@@ -34,25 +34,22 @@ function processFile() {
     var num = 0;
     var Node = [];
     var Elem = [];
-    for (i = 1; i < content.length; i = i+9, num = num+1) { 
-        if (content[i] != '\n*Element'){
+    for (i = 1; i < content.length; i = i+10, num = num+1) { 
+        if (content[i] != '\r\n\r\n*Element'){
             //Node Objects are created and characterized here
             Node[num] = { nodeName : 'Node'+ String(Number(content[i])), x : Number(content[i+1]),
                          y : Number(content[i+2]), z : Number(content[i+3]),
                          fixedX : Number(content[i+4]), fixedY : Number(content[i+5]),
-                         forceX : Number(content[i+6]), forceY : Number(content[i+7]),
-                         fdist : Number(content[i+7])};
-            //NodeObj =  document.createElement('a-entity');
-            //NodeObj.setAttribute('Node','NodeNum :' Number(content[i])';' 'Pos:  '{Number(content[i+1]) Number(content[i+2]) Number(content[i+3])})
-            //this.scene.appendChild(NodeObj)
+                         fixedM : Number(content[i+6]), forceX : Number(content[i+7]), 
+                         forceY : Number(content[i+8]), fdist : Number(content[i+9])};
 
 
         }
-        else if(content[i] == '\n*Element'){ 
+        else if(content[i] == '\r\n\r\n*Element'){ 
             for (var i = i + 1, num = 0; i < content.length; i = i+3, num = num+1) { 
                 //Element Objects are created and characterized here
-                Elem[num] = { elemName : 'Elem'+ String(Number(content[i])), nodeA : Number(content[i+1]),
-                             nodeB : Number(content[i+2])}};
+                Elem[num] = { elemName : 'Elem'+ String(Number(content[i])), 
+                             nodeA : Number(content[i+1]), nodeB : Number(content[i+2])}};
             //break;
         }
     }
