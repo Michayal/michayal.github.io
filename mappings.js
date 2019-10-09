a25b34077b6328f365105ea8df80351d11ec560b
michayal <michayal@tamu.edu>
2019-10-09 10:54:46 -0500

    commitv1

--- mappings.js
+++ mappings.js
@@ -1,20 +1,57 @@
-var mappings = {
-  mappings: {
-    default: {
-      common: {
-        trackpaddown: 'teleportstart',
-        trackpadup: 'teleportend'
-      },
-      'oculus-touch-controls': {
-        thumbstickdown: 'teleportstart',
-        thumbstickup: 'teleportend'
-      },
-      keyboard: {
-        't_down': 'teleportstart',
-        't_up': 'teleportend'
-      }
-    }
-  }
-};
+{
+var scene = document.querySelector('a-scene');
 
-AFRAME.registerInputMappings(mappings);
+if (scene.hasLoaded) {
+  run();
+} else {
+  scene.addEventListener('loaded', run);
+}
+
+function run () {
+  var t=
+  {behaviours:{},
+   
+   mappings:
+   {
+     painting:
+       {common:
+        {
+          "grip.down":"undo",
+          "trigger.changed":"paint"
+        },
+        
+        "vive-controls":
+        {
+          "axis.move":"changeBrushSizeInc",
+          "trackpad.touchstart":"startChangeBrushSize",
+          "menu.down":"toggleMenu",
+          "trackpad.down":"aim",
+          "trackpad.up":"teleport"
+        },
+        
+        "oculus-touch-controls":
+        {
+          "axis.move":"changeBrushSizeAbs",
+          "abutton.down":"toggleMenu",
+          "xbutton.down":"toggleMenu",
+          "ybutton.down":"aim",
+          "ybutton.up":"teleport",
+          "bbutton.down":"aim",
+          "bbutton.up":"teleport"
+        },
+        
+        "windows-motion-controls":
+        {
+          "axis.move":"changeBrushSizeAbs",
+          "menu.down":"toggleMenu",
+          "trackpad.down":"aim",
+          "trackpad.up":"teleport"
+        }
+       }
+   }
+  };
+
+
+  AFRAME.registerInputMappings(t),AFRAME.currentInputMapping="painting";
+}
+}
